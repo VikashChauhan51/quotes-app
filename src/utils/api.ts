4b1e9df69d278058366e5741c36504a7e67d286b
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-03-12 23:25:36 +0530

    fixed api endpoint format issue

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,21 +7,21 @@ export const getQuotes = async () => {
 };
 
 export const getQuote = async (id: string) => {
-  const response = await HttpClient.get(`$api/v1/${id}/quote`);
+  const response = await HttpClient.get(`api/v1/${id}/quote`);
   return response.data;
 };
 
 export const createQuote = async (quote: { message: string }) => {
-  const response = await HttpClient.post(`$api/v1/quote`, quote);
+  const response = await HttpClient.post(`api/v1/quote`, quote);
   return response.data;
 };
 
 export const updateQuote = async (quote: { id: string; message: string }) => {
-  const response = await HttpClient.put(`$api/v1/${quote.id}/quote`, quote);
+  const response = await HttpClient.put(`api/v1/${quote.id}/quote`, quote);
   return response.data;
 };
 
 export const deleteQuote = async (id: string) => {
-  const response = await HttpClient.delete(`$api/v1/${id}/quote`);
+  const response = await HttpClient.delete(`api/v1/${id}/quote`);
   return response.data;
 };
